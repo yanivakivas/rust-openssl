236dddd9a5e4a270c811ef9639ffefd951d82fba
yanivakivas <119865708+yanivakivas@users.noreply.github.com>
2023-06-05 18:13:16 +0300

    Update lib.rs

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 #![allow(dead_code, overflowing_literals, unused_imports)]
-#![doc(html_root_url = "https://docs.rs/openssl-sys/0.9")]
+#![doc(html_root_url = "https://docs.rs/openssl-sys/0.9.55")]
 
 extern crate libc;
 
